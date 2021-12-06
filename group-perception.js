b3a34812f0dfb8d6f02cfeda1575d4148de5bd79
reonZ <651462+reonZ@users.noreply.github.com>
2021-12-06 16:14:24 +0100

    added group perception macro

---
+++ group-perception.js
@@ -0,0 +1,18 @@
+/** @param {Token} token */
+function rollPerception(token) {
+    const mod = token.actor.data.data.attributes.perception.value
+    const roll = new Roll('1d20 + @mod', { mod }).evaluate({ async: false })
+    const die = roll.dice[0].total
+    let result = `<div class="flexrow"><span>${token.actor.name}</span><span class="flex0"`
+    if (die == 20) result += ' style="color: green;"'
+    else if (die == 1) result += ' style="color: red;"'
+    return `${result}>${roll.total}</span></div>`
+}
+
+function groupPerception() {
+    let result = '<hr>'
+    canvas.tokens.placeables.filter(x => x.actor?.hasPlayerOwner).forEach(token => (result += rollPerception(token)))
+    ChatMessage.create({ content: result, blind: true, flavor: 'Group Perception Checks' })
+}
+
+groupPerception()

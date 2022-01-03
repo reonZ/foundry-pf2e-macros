ea048bbf1c1264d2a32a47b575b702359114a1cf
reonZ <651462+reonZ@users.noreply.github.com>
2022-01-04 00:49:47 +0100

    perception rank is now displayed

--- group-perception.js
+++ group-perception.js
@@ -1,9 +1,29 @@
+/**
+ * @param {number} rank
+ */
+function skillRankToProficiency(rank) {
+    switch (rank) {
+        case 1:
+            return 'trained'
+        case 2:
+            return 'expert'
+        case 3:
+            return 'master'
+        case 4:
+            return 'legendary'
+        default:
+            return 'untrained'
+    }
+}
+
 /** @param {Token} token */
 function rollPerception(token) {
-    const mod = token.actor.data.data.attributes.perception.value
+    const perception = token.actor.data.data.attributes.perception
+    const mod = perception.value
+    const rank = skillRankToProficiency(perception.rank)
     const roll = new Roll('1d20 + @mod', { mod }).evaluate({ async: false })
     const die = roll.dice[0].total
-    let result = `<div class="flexrow"><span>${token.actor.name}</span><span class="flex0"`
+    let result = `<div class="flexrow"><span>${token.actor.name} (${rank})</span><span class="flex0"`
     if (die == 20) result += ' style="color: green;"'
     else if (die == 1) result += ' style="color: red;"'
     return `${result}>${roll.total}</span></div>`
